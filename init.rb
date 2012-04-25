647166b1c715186ca9421cf1cc2416697dfbf315
Jason Adams <jasonmadams@gmail.com>
2012-04-24 21:03:34 -0400

    change around loading

---
+++ init.rb
@@ -0,0 +1,15 @@
+require 'typhoeus'
+require 'nokogiri'
+require 'awesome_print'
+require 'active_support/core_ext/hash/conversions'
+
+require File.join(File.dirname(__FILE__), 'lib', 'neweden', 'errors')
+require File.join(File.dirname(__FILE__), 'lib', 'neweden', 'account')
+require File.join(File.dirname(__FILE__), 'lib', 'neweden', 'api')
+require File.join(File.dirname(__FILE__), 'lib', 'neweden', 'character')
+require File.join(File.dirname(__FILE__), 'lib', 'neweden', 'corporation')
+require File.join(File.dirname(__FILE__), 'lib', 'neweden', 'eve')
+require File.join(File.dirname(__FILE__), 'lib', 'neweden', 'image')
+require File.join(File.dirname(__FILE__), 'lib', 'neweden', 'map')
+require File.join(File.dirname(__FILE__), 'lib', 'neweden', 'server')
+require File.join(File.dirname(__FILE__), 'lib', 'neweden.rb')
